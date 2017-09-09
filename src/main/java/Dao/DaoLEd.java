331f8e0cfdef42a3c5cf8ce3e56c7a865bae67e1
Carlos <Carlos@DESKTOP-QKM8J15>
2017-09-09 18:35:33 -0500

    Server Java

---
+++ src/main/java/Dao/DaoLEd.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Carlos
+ */
+public class DaoLEd {
+   public void Encendido(){
+       String url = "jdbc:mysql://localhost:3306/Edison?useLegacyDatetimeCode=false&serverTimezone=UTC";
+        String username = "root";
+        String password = "root";
+        try (Connection connection = (Connection) DriverManager.getConnection(url, username, password)) {
+            String query = " insert into Led (estado)"
+        + " values (?)";
+        PreparedStatement preparedStmt = connection.prepareStatement(query);
+        preparedStmt.setInt(1, 1);
+        preparedStmt.execute();
+        connection.close();
+        System.out.println("Database connected!");
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
+   }
+   public void Apagado(){
+              String url = "jdbc:mysql://localhost:3306/Edison";
+        String username = "root";
+        String password = "root";
+        try (Connection connection = (Connection) DriverManager.getConnection(url, username, password)) {
+            String query = " insert into Led (estado)"
+        + " values (?)";
+        PreparedStatement preparedStmt = connection.prepareStatement(query);
+        preparedStmt.setInt(1, 1);
+        preparedStmt.execute();
+        connection.close();
+        System.out.println("Database connected!");
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
+   }
+}
